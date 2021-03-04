0bc75dbc2fa60efd746bbe89dbaa5e8bd0a4b23a
Salvador Sierra <salvilopezronda@gmail.com>
2021-03-04 14:41:53 +0100

    add isEvent() test.md

--- src/test/java/com/example/EventTest.java
+++ src/test/java/com/example/EventTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,10 +8,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class EventTest {
 
     @Test
-    void addAttendee() {
+    @DisplayName("isEvent")
+    void isEvent() {
+        Event event= new Event();
+        assertTrue(event instanceof Event);
     }
 
-    @Test
-    void addAttendees() {
-    }
+
 }
